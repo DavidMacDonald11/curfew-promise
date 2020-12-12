fb7cbaba880f7d1a5aa88fc819e7fd3d2e8de561
David <davidjmacdonald11@gmail.com>
2020-12-11 20:36:33 -0500

    Refactor to remove unnecessary code (UNTESTED)

--- index.js
+++ index.js
@@ -1,37 +1,9 @@
-const defaultOptions = {
-    curfew: 1000,
-    checks: 10,
-    wait: 1000 / 10
+module.exports = (curfew, func, then = undefined, thenCatch = undefined, ...funcArgs) => {
+    return new Promise(async (resolve, reject) => {
+        new Promise(async () => { resolve(await func(...funcArgs)); });
+
+        await new Promise((r) => { setTimeout(r, curfew); });
+
+        reject("Curfew of " + curfew + "ms has expired.");
+    }).then(then, thenCatch);
 };
-
-module.exports = (customOptions = undefined) => {
-    options = getOptions(customOptions);
-
-    return (func, then = undefined, thenCatch = undefined, ...funcArgs) => {
-        return new Promise(async (resolve, reject) => {
-            new Promise(async () => { resolve(await func(...funcArgs)); });
-
-            let checks = 0;
-
-            while(checks++ < options.checks)
-                await new Promise((r) => { setTimeout(r, options.wait); });
-
-            reject("Curfew of " + options.curfew + "ms has expired.");
-        }).then(then, thenCatch);
-    };
-}
-
-function getOptions(customOptions) {
-    if(typeof customOptions !== "object") return defaultOptions;
-
-    const customCurfew = typeof customOptions.curfew === "number";
-    const customChecks = typeof customOptions.checks === "number";
-
-    const options = {};
-
-    options["curfew"] = (customCurfew) ? customOptions.curfew : defaultOptions.curfew;
-    options["checks"] = (customChecks) ? customOptions.checks : defaultOptions.checks;
-    options["wait"] = options.curfew / options.checks;
-
-    return options;
-}
