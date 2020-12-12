728a71e168e35c968123c4f5a32b7f94ce5db1ca
David <davidjmacdonald11@gmail.com>
2020-12-12 09:09:45 -0500

    Continue refactor (UNTESTED)

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 module.exports = (curfew, func, then = undefined, thenCatch = undefined, ...funcArgs) => {
     return new Promise(async (resolve, reject) => {
-        new Promise(async () => { resolve(await func(...funcArgs)); });
-
-        await new Promise((r) => { setTimeout(r, curfew); });
-
-        reject("Curfew of " + curfew + "ms has expired.");
+        setTimeout(reject, curfew, "Curfew of " + curfew + "ms has expired.");
+        resolve(await func(...funcArgs)); 
     }).then(then, thenCatch);
 };
